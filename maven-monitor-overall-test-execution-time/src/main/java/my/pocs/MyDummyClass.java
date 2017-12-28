13796a99d0e0346b4719dcadc0426eaa99d4b820
Ajey Dudhe <ajeydudhe@gmail.com>
2017-12-28 11:41:58 +0530

    POC to fail the build if overall test execution time exceeds the threshold.

---
+++ maven-monitor-overall-test-execution-time/src/main/java/my/pocs/MyDummyClass.java
@@ -0,0 +1,20 @@
+/********************************************************************
+ * File Name:    MyDummyClass.java
+ *
+ * Date Created: Dec 28, 2017
+ *
+ * ------------------------------------------------------------------
+ * 
+ * Copyright (c) 2017 ajeydudhe@gmail.com
+ *
+ *******************************************************************/
+
+package my.pocs;
+  
+public class MyDummyClass
+{
+  public void doesNothing()
+  {    
+  }
+}
+
---
+++ maven-monitor-overall-test-execution-time/src/test/java/my/pocs/MyDummy02Test.java
@@ -0,0 +1,32 @@
+/********************************************************************
+ * File Name:    MyDummy02Test.java
+ *
+ * Date Created: Dec 28, 2017
+ *
+ * ------------------------------------------------------------------
+ * 
+ * Copyright (c) 2017 ajeydudhe@gmail.com
+ *
+ *******************************************************************/
+
+package my.pocs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyDummy02Test
+{
+  @Test
+  public void quickTest()
+  {
+    assertTrue(true != false);
+  }
+
+  @Test
+  public void longTest() throws InterruptedException
+  {
+    Thread.sleep(1000 * 10);
+  }
+}
+
